b7782b95be784ae207a6fed07ae18ddb94bdeafa
Aryan Singh <aryansingh81104@gmail.com>
2025-05-28 22:20:35 +0530

    Added New Cetificate for participation Download

--- certificate.php
+++ certificate.php
@@ -15,7 +15,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
     <link rel="manifest" href="images/favicon_io/site.webmanifest">
-    <title>Download NEO 2022 Certificates</title>
+    <title>Download NEO 2025 Certificates</title>
 </head>
 <style>
     .box1{
@@ -46,7 +46,7 @@
                 <img src="participation_certificate/cert_template.jpg" width="70%" alt="">
             </div>
             <div class="col-12 col-md-9 text-center">
-                <h4>Download the <b>PARTICIPATION CERTIFICATE</b> for NEO 2022</h4>
+                <h4>Download the <b>PARTICIPATION CERTIFICATE</b> for NEO 2025</h4>
                 <p><b>**Available only for the students who are registered and gave Round 1 Examination</b></p>
                     <a class="btn btn-dark bg btn1 btn" href="participation_certificate/" target="_blank" role="button"> DOWNLOAD PARTICIPATION CERTIFICATE<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
                       <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
@@ -54,18 +54,18 @@
             </div>
         </div>
 
-        <div class="row text-center py-5 box2">
+        <!-- <div class="row text-center py-5 box2">
             <div class="col-12 col-md-3 my-3" >
                 <img src="excellence_certificate/cert_template.jpg" width="70%" alt="">
             </div>
             <div class="col-12 col-md-9 text-center">
-                <h4>Download the <b>EXCELLENCE CERTIFICATE</b> for NEO 2022</h4>
+                <h4>Download the <b>EXCELLENCE CERTIFICATE</b> for NEO 2025</h4>
                 <p><b>**Available only for the students who cleared Round 1 Examination</b></p>
                     <a class="btn btn-dark bg btn1 btn12 btn" href="excellence_certificate/" target="_blank" role="button"> DOWNLOAD EXCELLENCE CERTIFICATE<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
                       <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                     </svg></a>
             </div>
-        </div>
+        </div> -->
         <div class="text-white mt-3 text-center"><h4>For any Queries related to certificates, Do mail your issue on <a class="foot" href="mailto:neo.pr.ecellvnit@gmail.com">neo.pr.ecellvnit@gmail.com</a></h4></div>
     </div>
 
--- participation_certificate/filter.php
+++ participation_certificate/filter.php
@@ -1,70 +1,67 @@
+
 <?php
 require('fpdf.php');
-// $server = '127.0.0.1';
-// $username = 'root';
-// $password = '';
-// $dbname = 'neo21';
+ini_set('memory_limit', '1024M');
 
-// to store the connection
-// $con = mysqli_connect($server, $username, $password, $dbname);
-$con = mysqli_connect("localhost","ias2020","ecell123","neo21");
-
-// to check whether the db is connected or not
-if (!$con) {
-  die("connection failed due to " . mysqli_connect_error());
+function sanitizeFileName($string) {
+    return preg_replace('/[^A-Za-z0-9_\-]/', '_', $string);
 }
-// check whether the download button is clicked or not
-if (isset($_POST['certificate_form'])) {
-  // store the name
-  $name = mysqli_real_escape_string($con, $_POST['name']);
-  // store the contact
-  $contact = mysqli_real_escape_string($con, $_POST['contact']);
-  // query to find all the records having given contact details
-  $checkUser = "SELECT * FROM finalNEO21entries where contact = '$contact'";
-  // querying the database
-  $result = mysqli_query($con, $checkUser);
-  // count the no. of record found
-  $count = mysqli_num_rows($result);
-  // if count is less than '1' which means no record was found
-  if ($count < 1) {
-    // record was not found
-    header('LOCATION:nouser.php');
-  } else {
-    // $table = $result->fetch_all(MYSQLI_ASSOC);
-    // $name = $table[0]['name'];
-    $name = ucwords($name);
-    header('content-type:application/pdf');
-    $image = @imagecreatefromjpeg("cert_template.jpg");
-    $output = "certificate.jpg";
+
+function generateCertificate($name) {
+    $templatePath = 'cert_template.jpg'; // Your uploaded certificate background
+    $fontPath = __DIR__ . '/Belleza.ttf'; // Font file
+
+    // Load certificate template
+    $image = imagecreatefromjpeg($templatePath);
     $black = imagecolorallocate($image, 0, 0, 0);
+    $fontSize =380; // Adjust font size as needed
 
-    $rotation = 0;
-    $font_size = 80;
-    $certificate_text = $name;
-    $drFont = realpath("Belleza.ttf");
+    // Calculate name position
+    $bbox = imagettfbbox($fontSize, 0, $fontPath, $name);
+    $x = 8299;
+    $y = 5100; // Adjust Y-position as needed
 
-    $txt_space = imagettfbbox($font_size, $rotation, $drFont, $certificate_text);
-    $txt_width = abs($txt_space[4] - $txt_space[0]);
-    $txt_height = abs($txt_space[3] - $txt_space[1]);
+    // Add name to image
+    imagettftext($image, $fontSize, 0, $x, $y, $black, $fontPath, $name);
 
-    $image_width = imagesx($image);
-    $image_height = imagesy($image);
-
-    $origin_x = abs($image_width - $txt_width) / 2;
-    $origin_y = 790;
-
-    $output = $name . ".jpeg";
-    imagettftext($image, $font_size, $rotation, $origin_x, $origin_y, $black, $drFont, $certificate_text);
-    imagejpeg($image, $output, 100);
+    // Save temp image
+    $cleanName = sanitizeFileName($name);
+    $tempJpg = "cert_$cleanName.jpg";
+    imagejpeg($image, $tempJpg);
     imagedestroy($image);
 
-    $pdf = new FPDF();
-    $pdf->AddPage('L', 'A4');
-    $pdf->Image($output, 0, 0, 300, 210);
-    $pdf->Output('D', "NEO_certificate_$name.pdf");
+    // Generate PDF
+    $pdf = new FPDF('L', 'mm', 'A4');
+    $pdf->AddPage();
+    $pdf->Image($tempJpg, 0, 0, 297, 210);
+    $pdfOutput = "NEO25_Certificate_$cleanName.pdf";
+    $pdf->Output('D', $pdfOutput);
 
-    if (is_file($output)) {
-      unlink($output);
-    }
-  }
+    unlink($tempJpg); // Clean up
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['certificate_form'])) {
+    $nameInput = trim($_POST['name']);
+    $phoneInput = trim($_POST['contact']);
+
+    $found = false;
+
+    if (($handle = fopen('data.csv', 'r')) !== false) {
+        fgetcsv($handle); // skip header
+        while (($data = fgetcsv($handle)) !== false) {
+            if ($data[1] == $phoneInput) {
+                $found = true;
+                $name = $data[0]; // Get official name from CSV
+                break;
+            }
+        }
+        fclose($handle);
+    }
+
+    if ($found) {
+        generateCertificate($name);
+    } else {
+        echo "<h3 style='color:red; text-align:center;'>Phone number not found in our records. Please check and try again.</h3>";
+    }
+}
+?>
